5cdebed7563e4b8c3eb0aa7b544f33665e4b91b6
WSzewczykSpD <wszewczyk.spd@gmail.com>
2025-01-16 10:17:22 +0100

    enemy boss health bar, hit react ability

--- Source/Warrior/Private/Component/UI/EnemyUIComponent.cpp
+++ Source/Warrior/Private/Component/UI/EnemyUIComponent.cpp
@@ -3,3 +3,25 @@
 
 #include "Component/UI/EnemyUIComponent.h"
 
+#include "Widget/WarriorWidgetBase.h"
+
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyUIComponent::RemoveEnemyDrawnWidgetsIfAny()
+{
+	if(EnemyDrawnWidgets.IsEmpty())
+	{
+		return;
+	}
+
+	for(UWarriorWidgetBase* DrawnWidget : EnemyDrawnWidgets)
+	{
+		if(DrawnWidget)
+		{
+			DrawnWidget->RemoveFromParent();
+		}
+	}
+}
